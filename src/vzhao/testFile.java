b261f414c1ba3bac4894b1883ae618d3b22b7672
BT_1N3_01 <vzhao2047@bths.edu>
2018-09-18 10:16:57 -0400

    End of class

--- src/vzhao/testFile.java
+++ src/vzhao/testFile.java
@@ -1,11 +1,15 @@
 package vzhao;
 
-import static vzhao.vzhaoLib.println;
+import static vzhao.vzhaoLib.date;
+import static vzhao.vzhaoLib.cutOut;
+import static vzhao.vzhaoLib.multiplyTable;
 
 public class testFile
 {
-    public static void main(String[] args)
-    {
-        println("I love a cup of Java in the morning.");
-    }
+   public static void main(String[] args)
+   {
+       System.out.println(date("12/01/2003"));
+       System.out.println((cutOut("catatonic cat","cat")));
+       multiplyTable(3,9);
+   }
 }
--- src/vzhao/vzhaoLib.java
+++ src/vzhao/vzhaoLib.java
@@ -2,8 +2,27 @@ package vzhao;
 
 public class vzhaoLib
 {
-    public static void println(String str)
+    public static String date(String date)
     {
-        System.out.println(str);
+        String d = date.substring(3,5);
+        String m = date.substring(0,2);
+        String y = date.substring(6,date.length());
+        return d+  "-"  +m+  "-"  +y;
     }
+    public static String cutOut(String mainStr, String subStr)
+    {
+        int index =mainStr.indexOf(subStr);
+        String a = mainStr.substring(index+3,mainStr.length());
+        return a;
+    }
+    public static void multiplyTable(int base,int range)
+    {
+        for(int x=0;range>=x;x++)
+        {
+            int y= x*base;
+            System.out.println(y);
+        }
+    }
+
+
 }
